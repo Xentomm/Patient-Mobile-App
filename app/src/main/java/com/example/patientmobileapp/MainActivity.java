24693f824c70f0c327697ce1b09f9628d2952d60
Xentomm <erimuslukasz@gmail.com>
2024-03-08 13:35:14 +0100

    calendar

--- app/src/main/java/com/example/patientmobileapp/MainActivity.java
+++ app/src/main/java/com/example/patientmobileapp/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.patientmobileapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
